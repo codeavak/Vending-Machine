f10c48c797d5ece09c20ac95e01c49a578af24c7
Johnny Avakian <javak@yahoo.com>
2018-03-29 01:05:13 -0700

    WIP2

--- Form1.cs
+++ Form1.cs
@@ -14,8 +14,10 @@ namespace drink
     {
 
         Beverage Cola;
-        Beverage CreamSoda; Beverage GrapeSoda;
-        Beverage LemonLime; Beverage RootBeer;
+        Beverage CreamSoda;
+        Beverage GrapeSoda;
+        Beverage LemonLime;
+        Beverage RootBeer;
         double total = 0;
         public void UpdateTotal()
         {
@@ -29,13 +31,13 @@ namespace drink
             txtAmtLemonLime.Text = $"{LemonLime.NumberRemaining}";
             txtAmtRootBeer.Text = $"{RootBeer.NumberRemaining}";
         }
-            
+
         public VendingMachine()
         {
             InitializeComponent();
             Cola = new Beverage("Cola", Convert.ToDouble(txtAmtCola.Tag));
             CreamSoda = new Beverage("Cream Soda", Convert.ToDouble(txtAmtCreamSoda.Tag));
-             GrapeSoda = new Beverage("Grape Soda", Convert.ToDouble(txtAmtGrapeSoda.Tag));
+            GrapeSoda = new Beverage("Grape Soda", Convert.ToDouble(txtAmtGrapeSoda.Tag));
             LemonLime = new Beverage("Lemon Lime", Convert.ToDouble(txtAmtLemonLime.Tag));
             RootBeer = new Beverage("Root Beer", Convert.ToDouble(txtAmtRootBeer.Tag));
 
@@ -51,19 +53,39 @@ namespace drink
 
         struct Beverage
         {
-            public string Name { get; set; }
-            public double Cost { get; set; }
-            public int NumberRemaining { get; set; }
-            public Beverage(string name,double cost)
+            public string Name
+            {
+                get;
+                set;
+            }
+            public double Cost
+            {
+                get;
+                set;
+            }
+            public int NumberRemaining
+            {
+                get;
+                set;
+            }
+            public Beverage(string name, double cost)
             {
                 this.Name = name;
                 this.Cost = cost;
                 this.NumberRemaining = 20;
             }
             public bool Dispense()
-            {if (NumberRemaining > 0)
-                { NumberRemaining--; return true; }
-                else { MessageBox.Show("No more cans of this beverage are left in the machine. Please try again later!"); return false; }
+            {
+                if (NumberRemaining > 0)
+                {
+                    NumberRemaining--;
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("No more cans of this beverage are left in the machine. Please try again later!");
+                    return false;
+                }
 
             }
         }
@@ -77,14 +99,34 @@ namespace drink
         {
 
             PictureBox clickedPictureBox = sender as PictureBox;
-       
+
             switch (clickedPictureBox.Name)
             {
-                case "pictureBox1": {if( Cola.Dispense()) total += Cola.Cost; UpdateTotal(); UpdateCounts(); } break;
-                case "pictureBox2": { if (CreamSoda.Dispense()) total += CreamSoda.Cost; UpdateTotal(); UpdateCounts(); } break;
-                case "pictureBox3": { if (GrapeSoda.Dispense()) total += GrapeSoda.Cost; UpdateTotal(); UpdateCounts(); } break;
-                case "pictureBox4": { if (LemonLime.Dispense()) total += LemonLime.Cost; UpdateTotal(); UpdateCounts(); } break;
-                case "pictureBox5": { if (RootBeer.Dispense()) total += RootBeer.Cost; UpdateTotal(); UpdateCounts(); } break;
+                case "pictureBox1":
+                    {
+                        if (Cola.Dispense()) total += Cola.Cost; UpdateTotal(); UpdateCounts();
+                    }
+                    break;
+                case "pictureBox2":
+                    {
+                        if (CreamSoda.Dispense()) total += CreamSoda.Cost; UpdateTotal(); UpdateCounts();
+                    }
+                    break;
+                case "pictureBox3":
+                    {
+                        if (GrapeSoda.Dispense()) total += GrapeSoda.Cost; UpdateTotal(); UpdateCounts();
+                    }
+                    break;
+                case "pictureBox4":
+                    {
+                        if (LemonLime.Dispense()) total += LemonLime.Cost; UpdateTotal(); UpdateCounts();
+                    }
+                    break;
+                case "pictureBox5":
+                    {
+                        if (RootBeer.Dispense()) total += RootBeer.Cost; UpdateTotal(); UpdateCounts();
+                    }
+                    break;
             }
             //here you can check which button was clicked by the sender
         }
